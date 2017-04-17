a8ff1a94e743b1309c5054ad13a897185da948d0
enjoyneering <enjoyneering79@gmail.com>
2017-04-17 15:00:58 -0400

    Update romPattern.ino

--- examples/romPattern/romPattern.ino
+++ examples/romPattern/romPattern.ino
@@ -1,23 +1,27 @@
-/******************************************************************************/
+/***************************************************************************************************/
 /*
-  PCF8574 chip uses I2C bus to communicate, 2 pins are required to  
-  interface.
+  written by : enjoyneering79
+  sourse code: https://github.com/enjoyneering/
 
-  Connect PCF8574 to pins :  SDA     SCL
-  Uno, Mini, Pro:            A4      A5
-  Mega2560, Due:             20      21
-  Leonardo:                  2       3
-  ATtiny85:                  0(5)    2/A1(7) (TinyWireM   - https://github.com/SpenceKonde/TinyWireM & ATTinyCore - https://github.com/SpenceKonde/ATTinyCore)
-  ESP8266 ESP-xx:            ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:               ANY     ANY     (D1 & D2 by default)
+  This chip uses I2C bus to communicate. Two pins are required to interface
+
+  Connect chip to pins:    SDA     SCL
+  Uno, Mini, Pro:          A4      A5
+  Mega2560, Due:           20      21
+  Leonardo:                2       3
+  ATtiny85:                0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                            & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-xx:          ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             ANY     ANY     (D2 & D1 by default)
+
 */
-/******************************************************************************/
+/***************************************************************************************************/
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-uint8_t lcd_colums = 20;
-uint8_t lcd_rows   = 4;
-uint8_t lcd_page   = lcd_colums * lcd_rows;
+#define COLUMS 20
+#define ROWS   4
+#define PAGE   COLUMS * ROWS
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
@@ -25,22 +29,21 @@ void setup()
 {
   Serial.begin(115200);
 
-  if (lcd.begin(lcd_colums, lcd_rows) != 1)
+  while (lcd.begin(COLUMS, ROWS) != 1) //colums - 20, rows - 4
   {
     Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
+    delay(5000);   
   }
-  else
-  {
-    Serial.println("PCF8574 is OK...");
-  }
+  lcd.print("PCF8574 is OK...");
+  delay(2000);
+  lcd.clear();
 }
-
 
 void loop()
 {
   for (uint8_t i = 0; i < 256; i++)
   {
-    if (i != 0 && i % lcd_page == 0)
+    if (i != 0 && i % PAGE == 0)
     {
       delay(10000);
       lcd.clear();
