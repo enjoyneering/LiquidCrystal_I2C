37b6dbc3ef20e5a897def9a5c2b73838343be2b7
enjoyneering <enjoyneering79@gmail.com>
2017-04-17 14:59:21 -0400

    Update MultipleLCD.ino

--- examples/MultipleLCD/MultipleLCD.ino
+++ examples/MultipleLCD/MultipleLCD.ino
@@ -1,17 +1,21 @@
-/******************************************************************************/
+/***************************************************************************************************/
 /*
-  PCF8574 chip uses I2C bus to communicate, 2 pins are required to  
-  interface.
+  written by : enjoyneering79
+  sourse code: https://github.com/enjoyneering/
 
-  Connect PCF8574 to pins :  SDA     SCL
-  Uno, Mini, Pro:            A4      A5
-  Mega2560, Due:             20      21
-  Leonardo:                  2       3
-  ATtiny85:                  0(5)    2/A1(7) (TinyWireM   - https://github.com/SpenceKonde/TinyWireM & ATTinyCore - https://github.com/SpenceKonde/ATTinyCore)
-  ESP8266 ESP-xx:            ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:               ANY     ANY     (D1 & D2 by default)
+  This chip uses I2C bus to communicate, 2 pins are required to interface
+
+  Connect chip to pins:    SDA     SCL
+  Uno, Mini, Pro:          A4      A5
+  Mega2560, Due:           20      21
+  Leonardo:                2       3
+  ATtiny85:                0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                            & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-xx:          ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             ANY     ANY     (D2 & D1 by default)
+
 */
-/******************************************************************************/
+/***************************************************************************************************/
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
  
@@ -23,36 +27,26 @@ void setup()
 {
   Serial.begin(115200);
 
-  if (lcd_01.begin(20, 4) != 1) //colums - 20, rows - 4
+  while (lcd_01.begin(20, 4) != 1 || lcd_02.begin(16, 2) != 1) //lcd1: colums - 20, rows - 4, lcd2: colums - 16, rows - 2
   {
-    Serial.println("PCF8574_01 is not connected or lcd pins declaration is wrong. Only pins numbers 4,5,6,16,11,12,13,14 are legal.");
+    Serial.println("PCF8574_01 or PCF8574_02 is not connected or lcd pins declaration is wrong. Only pins numbers 4,5,6,16,11,12,13,14 are legal.");
+    delay(5000);
   }
-  else
-  {
-    lcd_01.print("PCF8574_01 is OK...");
-  }
+  lcd_01.print("PCF8574_01 is OK...");
+  lcd_02.print("PCF8574_02 is OK...");
 
-  if (lcd_02.begin(16, 2) != 1) //colums - 16, rows - 2
-  {
-    Serial.println("PCF8574_02 is not connected or lcd pins declaration is wrong. Only pins numbers 4,5,6,16,11,12,13,14 are legal.");
-  }
-  else
-  {
-    lcd_02.print("PCF8574_02 is OK...");
-  }
+
+  lcd_01.setCursor(0, 1);              //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
+  lcd_01.print("LCD1 Hello world");
+
+  lcd_02.setCursor(0, 1);              //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
+  lcd_02.print("LCD2 Hello world");
 }
 
 void loop()
 {
-   lcd_01.setCursor(0, 1);           //set first colum & second row. NOTE: first colum & row started at zero
-   lcd_01.print("LCD1 Hello world");
-
-   lcd_01.setCursor(0, 2);           //set first colum & third  row. NOTE: first colum & row started at zero
-   lcd_01.print("Random number:");
-   lcd_01.print(random(10, 100));
-
-   lcd_02.setCursor(0, 1);           //set first colum & second row. NOTE: first colum & row started at zero
-   lcd_02.print("LCD2 Hello world");
-
-   delay(4000);
+  lcd_01.setCursor(0, 2);              //set 1-st colum & 3-rd  row. NOTE: 1-st colum & row started at zero
+  lcd_01.print("Random number:");
+  lcd_01.print(random(10, 100) + " ");
+  delay(4000);
 }
