fef650960bbc42dd85498e7e140cb0714f244f3a
enjoyneering <enjoyneering79@gmail.com>
2017-04-17 15:03:26 -0400

    Update SerialDisplay.ino

--- examples/SerialDisplay/SerialDisplay.ino
+++ examples/SerialDisplay/SerialDisplay.ino
@@ -1,52 +1,52 @@
-/******************************************************************************/
+/***************************************************************************************************/
 /*
-  PCF8574 chip uses I2C bus to communicate, 2 pins are required to  
-  interface.
+  PCF8574 chip uses I2C bus to communicate, 2 pins are required to interface
 
   Connect PCF8574 to pins :  SDA     SCL
   Uno, Mini, Pro:            A4      A5
   Mega2560, Due:             20      21
   Leonardo:                  2       3
-  ATtiny85:                  0(5)    2/A1(7) (TinyWireM   - https://github.com/SpenceKonde/TinyWireM & ATTinyCore - https://github.com/SpenceKonde/ATTinyCore)
+  ATtiny85:                  0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                              & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
   ESP8266 ESP-xx:            ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:               ANY     ANY     (D1 & D2 by default)
+  NodeMCU 1.0:               ANY     ANY     (D2 & D1 by default)
 */
-/******************************************************************************/
+/***************************************************************************************************/
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-uint8_t char_counter;
+#define COLUMS 20
+#define ROWS   4
+#define PAGE   COLUMS * ROWS
 
-uint8_t lcd_colums = 20;
-uint8_t lcd_rows   = 4;
-uint8_t lcd_page   = lcd_colums * lcd_rows;
+uint8_t char_counter = 0;
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.flush();
 
-  if (lcd.begin(lcd_colums, lcd_rows) != 1)
+  while (lcd.begin(COLUMS, ROWS) != 1) //colums - 20, rows - 4
   {
     Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
+    delay(5000);   
   }
-  else
-  {
-    Serial.println("PCF8574 is OK...");
-  }
+  lcd.print("PCF8574 is OK...");
+  delay(2000);
+  lcd.clear();
+
   lcd.blink();
-  lcd.print("Wake up, Neo...");
+ 
 }
 
 void loop()
 {
   while (Serial.available() > 0)
   {
-    char_counter++;
+    char_counter += Serial.available();
 
-    if (char_counter == lcd_page)
+    if (char_counter == PAGE)
     {
       char_counter = 0;
       lcd.clear();
