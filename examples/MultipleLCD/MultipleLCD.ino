ab921d3d5db1624d0e15f6d3c92c0760cafc56d1
enjoyneering <enjoyneering79@gmail.com>
2017-07-10 08:29:15 -0400

    Update MultipleLCD.ino

--- examples/MultipleLCD/MultipleLCD.ino
+++ examples/MultipleLCD/MultipleLCD.ino
@@ -1,19 +1,16 @@
 /***************************************************************************************************/
 /*
-  written by : enjoyneering79
-  sourse code: https://github.com/enjoyneering/
+  This is an Arduino sketch for LiquidCrystal_I2C library.
 
-  This chip uses I2C bus to communicate, 2 pins are required to interface
-
-  Connect chip to pins:    SDA     SCL
-  Uno, Mini, Pro:          A4      A5
-  Mega2560, Due:           20      21
-  Leonardo:                2       3
-  ATtiny85:                0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                            & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-xx:          ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             ANY     ANY     (D2 & D1 by default)
-
+  PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
+  Connect chip to pins:    SDA        SCL
+  Uno, Mini, Pro:          A4         A5
+  Mega2560, Due:           20         21
+  Leonardo:                2          3
+  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
 */
 /***************************************************************************************************/
 #include <Wire.h> 
@@ -34,19 +31,24 @@ void setup()
   }
   lcd_01.print("PCF8574_01 is OK...");
   lcd_02.print("PCF8574_02 is OK...");
+  delay(2000);
+  lcd_01.clear();
+  lcd_02.clear();
 
-
+  /* prints static text */
   lcd_01.setCursor(0, 1);              //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
-  lcd_01.print("LCD1 Hello world");
+    lcd_01.print("LCD1 Hello world");
+  lcd_01.setCursor(0, 2);              //set 1-st colum & 3-rd  row. NOTE: 1-st colum & row started at zero
+    lcd_01.print("Random number:");
 
   lcd_02.setCursor(0, 1);              //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
-  lcd_02.print("LCD2 Hello world");
+    lcd_02.print("LCD2 Hello world");
 }
 
 void loop()
 {
-  lcd_01.setCursor(0, 2);              //set 1-st colum & 3-rd  row. NOTE: 1-st colum & row started at zero
-  lcd_01.print("Random number:");
+  /* prints dynamic text */
+  lcd_01.setCursor(14, 2);             //set 15-th colum & 3-rd  row. NOTE: 1-st colum & row started at zero
   lcd_01.print(random(10, 100) + " ");
-  delay(4000);
+  delay(1000);
 }
