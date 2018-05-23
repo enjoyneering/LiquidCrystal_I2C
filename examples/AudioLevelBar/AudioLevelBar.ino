a0bd1b4bfb4b749cfc4d2640a9849f95bde0e8b3
enjoyneering <enjoyneering79@gmail.com>
2018-05-23 09:57:22 -0400

    Update AudioLevelBar.ino

--- examples/AudioLevelBar/AudioLevelBar.ino
+++ examples/AudioLevelBar/AudioLevelBar.ino
@@ -3,24 +3,35 @@
   This is an Arduino sketch for LiquidCrystal_I2C library.
 
   PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro........................... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo................................. 2                      3
+  Trinket/ATtiny85......................... 0/physical pin no.5    2/physical pin no.7
+  Blue Pill/STM32F103xxxx boards........... B7                     B6 with 5v->3v logich converter
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ATtiny I2C Master lib - https://github.com/SpenceKonde/TinyWireM
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-#define COLUMS 20
-#define ROWS   4
+#define COLUMS    20
+#define ROWS      4
 
-uint8_t l_channel = A0;
-uint8_t r_channel = A1;
+#define L_CHANNEL A0
+#define R_CHANNEL A1
+
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
@@ -33,13 +44,15 @@ void setup()
     Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
     delay(5000);   
   }
+
   lcd.print("PCF8574 is OK...");
   delay(2000);
+
   lcd.clear();
 }
 
 void loop()
 {
-  lcd.printHorizontalGraph('L', 0, analogRead(l_channel), 1023); //name of the bar, first  row, current value, max. value
-  lcd.printHorizontalGraph('R', 1, analogRead(r_channel), 1023); //name of the bar, second row, current value, max. value
+  lcd.printHorizontalGraph('L', 0, analogRead(L_CHANNEL), 1023); //name of the bar, first  row, current value, max. value
+  lcd.printHorizontalGraph('R', 1, analogRead(R_CHANNEL), 1023); //name of the bar, second row, current value, max. value
 }
