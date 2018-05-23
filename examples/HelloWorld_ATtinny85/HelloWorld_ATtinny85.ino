6c5563cdde63f7e2c72eb3592109066bf3011be9
enjoyneering <enjoyneering79@gmail.com>
2018-05-23 10:01:37 -0400

    Update HelloWorld_ATtinny85.ino

--- examples/HelloWorld_ATtinny85/HelloWorld_ATtinny85.ino
+++ examples/HelloWorld_ATtinny85/HelloWorld_ATtinny85.ino
@@ -3,41 +3,53 @@
   This is an Arduino sketch for LiquidCrystal_I2C library.
 
   PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro........................... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo................................. 2                      3
+  Trinket/ATtiny85......................... 0/physical pin no.5    2/physical pin no.7
+  Blue Pill/STM32F103xxxx boards........... B7                     B6 with 5v->3v logich converter
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ATtiny I2C Master lib - https://github.com/SpenceKonde/TinyWireM
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <TinyWireM.h>
+#include <TinyWireM.h>         //https://github.com/SpenceKonde/TinyWireM
 #include <LiquidCrystal_I2C.h>
 
 #define COLUMS 20
 #define ROWS   4
 
-uint8_t led = 1;  //connect led to ATtiny85 pin no.6 in series with 470 Ohm resistor
+#define LED    1               //connect led to ATtiny85 physical pin no.6 in series with 470 Ohm resistor
+#define LCD_SPACE_SYMBOL 0x20  //space symbol from the LCD ROM, see p.9 of GDM2004D datasheet
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 void setup()
 {
-  pinMode(led, OUTPUT);
+  pinMode(LED, OUTPUT);
 
   while (lcd.begin(COLUMS, ROWS) != 1) //colums - 20, rows - 4
   {
-    for (uint8_t i = 0; i > 3; i++)    //3 blinks, PCF8574/LCD is not connected or lcd pins declaration is wrong
+    for (uint8_t i = 0; i < 3; i++)    //3 blinks, PCF8574/LCD is not connected or lcd pins declaration is wrong
     {
-      digitalWrite(led, HIGH);
-      delay(400);
-      digitalWrite(led, LOW);
-      delay(400);
+      digitalWrite(LED, HIGH);
+      delay(500);
+      digitalWrite(LED, LOW);
+      delay(500);
     }
     delay(5000);
   }
+
   /* prints static text */
   lcd.print("Hello world!");
   lcd.setCursor(0, 1);                 //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
@@ -46,9 +58,10 @@ void setup()
 
 void loop()
 {
-  /* print dynamic text */
+  /* prints dynamic text */
   lcd.setCursor(14, 1);                //set 15-th colum & 2-nd row. NOTE: 1-st colum & row started at zero
-  lcd.print(random(10, 100));
+  lcd.print(random(10, 1000));
+  lcd.write(LCD_SPACE_SYMBOL);
 
   delay(1000);
 }
