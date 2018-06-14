069db3033594d4ede76ee649fd8156625bdbfdfe
enjoyneering <enjoyneering79@gmail.com>
2018-06-13 20:19:50 -0400

    Update MultipleLCD.ino

--- examples/MultipleLCD/MultipleLCD.ino
+++ examples/MultipleLCD/MultipleLCD.ino
@@ -40,12 +40,12 @@ void setup()
 
   while (lcd_01.begin(20, 4) != 1 || lcd_02.begin(16, 2) != 1) //lcd1: colums - 20, rows - 4, lcd2: colums - 16, rows - 2
   {
-    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps free dynamic memory
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
     delay(5000);
   }
 
-  lcd_01.print("PCF8574_01 is OK...");
-  lcd_02.print("PCF8574_02 is OK...");
+  lcd_01.print(F("PCF8574_01 is OK..."));
+  lcd_02.print(F("PCF8574_02 is OK..."));
   delay(2000);
 
   lcd_01.clear();
@@ -53,24 +53,24 @@ void setup()
 
   /* prints static text */
   lcd_01.setCursor(0, 1);              //set 1-st colum & 2-nd row, 1-st colum & row started at zero
-  lcd_01.print("LCD1 Hello world");
+  lcd_01.print(F("LCD1 Hello world"));
   lcd_01.setCursor(0, 2);
-  lcd_01.print("Random number:");
+  lcd_01.print(F("Random number:"));
 
   lcd_02.setCursor(0, 0);
-  lcd_02.print("LCD2 Hello world");
+  lcd_02.print(F("LCD2 Hello world"));
   lcd_02.setCursor(0, 1);
-  lcd_02.print("Random number:");
+  lcd_02.print(F("Random number:"));
 }
 
 void loop()
 {
   /* prints dynamic text */
-  lcd_01.setCursor(14, 2);             //set 15-th colum & 3-rd  row. NOTE: 1-st colum & row started at zero
+  lcd_01.setCursor(14, 2);             //set 15-th colum & 3-rd  row, 1-st colum & row started at zero
   lcd_01.print(random(10, 1000));
   lcd_01.write(LCD_SPACE_SYMBOL);
 
-  lcd_02.setCursor(14, 1);             //set 15-th colum & 2-rd  row. NOTE: 1-st colum & row started at zero
+  lcd_02.setCursor(14, 1);             //set 15-th colum & 2-rd  row, 1-st colum & row started at zero
   lcd_02.print(random(10, 1000));
   lcd_02.write(LCD_SPACE_SYMBOL);
 
