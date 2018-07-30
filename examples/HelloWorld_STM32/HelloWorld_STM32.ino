8ae0f9a8676571be5cfc3b35cd7e6cadd29318df
enjoyneering <enjoyneering79@gmail.com>
2018-07-29 21:20:14 -0400

    Update HelloWorld_STM32.ino

--- examples/HelloWorld_STM32/HelloWorld_STM32.ino
+++ examples/HelloWorld_STM32/HelloWorld_STM32.ino
@@ -2,26 +2,26 @@
 /*
   This is an Arduino sketch for LiquidCrystal_I2C library.
 
-  PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
-  Board:                                    SDA                    SCL
-  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
-  Mega2560, Due............................ 20                     21
-  Leonardo, Micro, ATmega32U4.............. 2                      3
-  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
-  Blue Pill, STM32F103xxxx boards.......... B7*                    B6*
-  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
-  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
-
-                                            *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
-                                             logic level converter is recommended
+  This chip uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL                    Level
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5                     5v
+  Mega2560................................. 20                     21                     5v
+  Due, SAM3X8E............................. 20                     21                     3.3v
+  Leonardo, Micro, ATmega32U4.............. 2                      3                      5v
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7    5v
+  Blue Pill, STM32F103xxxx boards.......... PB7                    PB6                    3.3v/5v
+  ESP8266 ESP-01........................... GPIO0/D5               GPIO2/D3               3.3v/5v
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1               3.3v/5v
+  ESP32.................................... GPIO21/D21             GPIO22/D22             3.3v
 
   Frameworks & Libraries:
   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP32 Core            - https://github.com/espressif/arduino-esp32
   ESP8266 Core          - https://github.com/esp8266/Arduino
   ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
-  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
@@ -31,7 +31,7 @@
 #define COLUMS           20
 #define ROWS             4
 
-#define LCD_SPACE_SYMBOL 0x20  //space symbol from the LCD ROM, see p.9 of GDM2004D datasheet
+#define LCD_SPACE_SYMBOL 0x20  //space symbol from the lcd ROM, see p.9 of GDM2004D datasheet
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
@@ -41,25 +41,25 @@ void setup()
 
   while (lcd.begin(COLUMS, ROWS, LCD_5x8DOTS) != 1) //colums - 20, rows - 4, pixels - 5x8, pins B6/B7 are 5v tolerant, but bi-directional logic level converter is recommended
   {
-    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
     delay(5000);
   }
 
-  lcd.print(F("PCF8574 is OK..."));
+  lcd.print(F("PCF8574 is OK...")); //(F()) saves string to flash & keeps dynamic memory free
   delay(2000);
 
   lcd.clear();
 
   /* prints static text */
   lcd.print(F("Hello world!"));
-  lcd.setCursor(0, 1);            //set 1-st colum & 2-nd row. NOTE: 1-st colum & row started at zero
+  lcd.setCursor(0, 1);              //set 1-st colum & 2-nd row, 1-st colum & row started at zero
   lcd.print(F("Random number:"));
 }
 
 void loop()
 {
   /* prints dynamic text */
-  lcd.setCursor(14, 1);           //set 15-th colum & 2-nd row. NOTE: 1-st colum & row started at zero
+  lcd.setCursor(14, 1);             //set 15-th colum & 2-nd row, 1-st colum & row started at zero
   lcd.print(random(10, 1000));
   lcd.write(LCD_SPACE_SYMBOL);
 
